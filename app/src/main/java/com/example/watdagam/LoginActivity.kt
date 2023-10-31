47149f77493e0c993119567dcd1712592d7b55ef
sangkkim <madlogic365@gmail.com>
2023-10-31 23:01:01 +0900

    Feat/WFA-30 WDG 로그인 로직 생성 - 200: 리스트 뷰 - 201: 회원가입 뷰 - 400: 로그인 거부 - other: unhandled

--- app/src/main/java/com/example/watdagam/LoginActivity.kt
+++ app/src/main/java/com/example/watdagam/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.watdagam
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,11 +11,20 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
 
 class LoginActivity : AppCompatActivity() {
 
@@ -52,12 +62,13 @@ class LoginActivity : AppCompatActivity() {
         val tag = "KakaoLogin"
 
         //로그인 성공시 호출될 함수
-        fun loginWithAccessToken(accessToken: String) {
+        fun kakaoLoginSuccess(accessToken: String) {
             Toast.makeText(this, "access token: $accessToken", Toast.LENGTH_SHORT).show()
+            loginWithKakaoToken(accessToken)
         }
 
         // 로그인 실패시 호출될 함수
-        fun loginFail() {
+        fun kakaoLoginFail() {
             Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
         }
 
@@ -66,10 +77,10 @@ class LoginActivity : AppCompatActivity() {
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
                 Log.e(tag, "카카오계정으로 로그인 실패", error)
-                loginFail()
+                kakaoLoginFail()
             } else if (token != null) {
                 Log.i(tag, "카카오계정으로 로그인 성공 ${token.accessToken}")
-                loginWithAccessToken(token.accessToken)
+                kakaoLoginSuccess(token.accessToken)
             }
         }
 
@@ -78,7 +89,7 @@ class LoginActivity : AppCompatActivity() {
             UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
                 if (error != null) {
                     Log.e(tag, "카카오톡으로 로그인 실패", error)
-                    loginFail()
+                    kakaoLoginFail()
 
                     // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                     // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
@@ -90,11 +101,71 @@ class LoginActivity : AppCompatActivity() {
                     UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
                 } else if (token != null) {
                     Log.i(tag, "카카오톡으로 로그인 성공 ${token.accessToken}")
-                    loginWithAccessToken(token.accessToken)
+                    kakaoLoginSuccess(token.accessToken)
                 }
             }
         } else {
             UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
         }
+    }
+
+    interface WDGLoginService {
+        @GET("login")
+        fun loginWithToken(
+            @Header("Authorization") token: String,
+            @Query("platform") platform: String
+        ): Call<Void>
+    }
+
+    private fun loginWithKakaoToken(accessToken: String) {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://0b88436b-a8a0-463a-bb4d-07b31d747be2.mock.pstmn.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val loginService: WDGLoginService = retrofit.create(WDGLoginService::class.java)
+        val loginCall = loginService.loginWithToken("Bearer $accessToken", "KAKAO")
+
+        loginCall.enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.isSuccessful.not()) {
+                    Log.e("WDG", "Fail ${response}")
+                } else {
+                    Log.d("WDG", "Success ${response.code()}")
+                    when (response.code()) {
+                        200 -> {
+                            // Move to list page
+                            val intent = Intent(this@LoginActivity, ListActivity::class.java)
+                            startActivity(intent)
+                        }
+
+                        201 -> {
+                            // Move to signup page
+                            val intent = Intent(this@LoginActivity, SignupActivity::class.java)
+                            startActivity(intent)
+                        }
+
+                        400 -> {
+                            // Invalid token (invalid or expired)
+                            val alertDialogBuilder = AlertDialog.Builder(this@LoginActivity)
+                            alertDialogBuilder.setTitle("로그인에 실패했습니다.")
+                            alertDialogBuilder.setMessage("다시 시도해주세요.")
+                            alertDialogBuilder.setPositiveButton("확인") { _, _ -> }
+                            alertDialogBuilder.create()
+                            alertDialogBuilder.show()
+                        }
+
+                        else -> {
+                            // unhandled case
+                            Log.e("ERROR", "unhandled response code${response.code()}")
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                call.cancel()
+                Log.e("ERROR", "http failure")
+            }
+        })
     }
 }
