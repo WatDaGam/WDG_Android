ae6d320e436bf79a71feaf43e092d951f69c94e7
sangkkim <madlogic365@gmail.com>
2023-10-31 22:50:32 +0900

    Chore/WFA-30 Retrofit 패키지 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,8 @@ dependencies {
     // 카카오 API
     implementation("com.kakao.sdk:v2-all:2.17.0") // 전체 모듈 설치, 2.11.0 버전부터 지원
     implementation("com.kakao.sdk:v2-user:2.17.0") // 카카오 로그인
+
+    // retrofit HTTP 통신
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
