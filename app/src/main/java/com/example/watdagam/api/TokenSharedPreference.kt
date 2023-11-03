16dac48c81b437c094d0a8eb67b0b82e42007c6a
sangkkim <madlogic365@gmail.com>
2023-11-03 12:47:21 +0900

    Feat/WFA-30 token 만료시간 추가

--- app/src/main/java/com/example/watdagam/api/TokenSharedPreference.kt
+++ app/src/main/java/com/example/watdagam/api/TokenSharedPreference.kt
@@ -4,16 +4,29 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class TokenSharedPreference(context: Context) {
-    private val prefFilename = "token_preferences"
-    private val keyAccessToken = "accessToken"
-    private val keyRefreshToken = "refreshToken"
-    private val pref: SharedPreferences = context.getSharedPreferences(prefFilename,0)
+
+    companion object {
+        private const val PREFERENCE_FILENAME = "wdg-token-preference"
+        private const val KEY_ACCESS_TOKEN = "access-token"
+        private const val KEY_ACCESS_TOKEN_EXPIRATION_TIME = "access-token-expiration-time"
+        private const val KEY_REFRESH_TOKEN = "refresh-token"
+        private const val KEY_REFRESH_TOKEN_EXPIRATION_TIME = "refresh-token-expiration-time"
+    }
+    private val preferences: SharedPreferences = context.getSharedPreferences(PREFERENCE_FILENAME,0)
 
     var accessToken: String?
-        get() = pref.getString(keyAccessToken, "")
-        set(value) = pref.edit().putString(keyAccessToken, value).apply()
+        get() = preferences.getString(KEY_ACCESS_TOKEN, "")
+        set(value) = preferences.edit().putString(KEY_ACCESS_TOKEN, value).apply()
+
+    var accessTokenExpirationTime: Long
+        get() = preferences.getLong(KEY_ACCESS_TOKEN_EXPIRATION_TIME, 0)
+        set(value) = preferences.edit().putLong(KEY_ACCESS_TOKEN_EXPIRATION_TIME, value).apply()
 
     var refreshToken: String?
-        get() = pref.getString(keyRefreshToken, "")
-        set(value) = pref.edit().putString(keyRefreshToken, value).apply()
+        get() = preferences.getString(KEY_REFRESH_TOKEN, "")
+        set(value) = preferences.edit().putString(KEY_REFRESH_TOKEN, value).apply()
+
+    var refreshTokenExpirationTime: Long
+        get() = preferences.getLong(KEY_REFRESH_TOKEN_EXPIRATION_TIME, 0)
+        set(value) = preferences.edit().putLong(KEY_REFRESH_TOKEN_EXPIRATION_TIME, value).apply()
 }
