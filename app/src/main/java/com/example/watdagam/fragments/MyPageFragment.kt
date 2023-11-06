e53cfcd87c48f1bac16f3d2eb4aea108cff26252
sangkkim <madlogic365@gmail.com>
2023-11-06 21:31:46 +0900

    Feat/WFA-53 마이페이지 헤더 레이아웃 생성

--- app/src/main/java/com/example/watdagam/fragments/MyPageFragment.kt
+++ app/src/main/java/com/example/watdagam/fragments/MyPageFragment.kt
@@ -3,58 +3,50 @@ package com.example.watdagam.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.watdagam.R
+import com.example.watdagam.databinding.FragmentMyPageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var viewBinding: FragmentMyPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
-    }
+        viewBinding = FragmentMyPageBinding.inflate(inflater, container, false)
+        // Title
+        viewBinding.toolbarTitle.text = "MyPage"
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        // Menu
+        viewBinding.toolbar.overflowIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_menu)
+        viewBinding.toolbar.inflateMenu(R.menu.my_page_menu)
+        viewBinding.toolbar.setOnMenuItemClickListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.logoutButton -> {
+                    TODO("call api")
+                }
+                R.id.withdrawalButton -> {
+                    TODO("call api")
+                }
+                else -> {
+                    TODO("call api")
                 }
             }
+        }
+
+        // Back
+        viewBinding.toolbar.setNavigationOnClickListener { view ->
+            TODO("move backward")
+        }
+        return viewBinding.root
     }
+
 }
