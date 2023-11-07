2bf45f73606c00884a0d66d5e658992a02dfae76
sangkkim <madlogic365@gmail.com>
2023-11-07 13:32:22 +0900

    Feat/WFA-45 token 캡슐화

--- app/src/main/java/com/example/watdagam/api/TokenSharedPreference.kt
+++ app/src/main/java/com/example/watdagam/api/TokenSharedPreference.kt
@@ -14,19 +14,44 @@ class TokenSharedPreference(context: Context) {
     }
     private val preferences: SharedPreferences = context.getSharedPreferences(PREFERENCE_FILENAME,0)
 
-    var accessToken: String?
+    private var accessToken: String?
         get() = preferences.getString(KEY_ACCESS_TOKEN, "")
         set(value) = preferences.edit().putString(KEY_ACCESS_TOKEN, value).apply()
 
-    var accessTokenExpirationTime: Long
+    private var accessTokenExpirationTime: Long
         get() = preferences.getLong(KEY_ACCESS_TOKEN_EXPIRATION_TIME, 0)
         set(value) = preferences.edit().putLong(KEY_ACCESS_TOKEN_EXPIRATION_TIME, value).apply()
 
-    var refreshToken: String?
+    private var refreshToken: String?
         get() = preferences.getString(KEY_REFRESH_TOKEN, "")
         set(value) = preferences.edit().putString(KEY_REFRESH_TOKEN, value).apply()
 
-    var refreshTokenExpirationTime: Long
+    private var refreshTokenExpirationTime: Long
         get() = preferences.getLong(KEY_REFRESH_TOKEN_EXPIRATION_TIME, 0)
         set(value) = preferences.edit().putLong(KEY_REFRESH_TOKEN_EXPIRATION_TIME, value).apply()
+
+    fun getAccessToken(): String {
+        if (accessTokenExpirationTime - System.currentTimeMillis() < 10_000) {
+            accessToken = ""
+            accessTokenExpirationTime = 0
+        }
+        return accessToken?: ""
+    }
+    fun setAccessToken(token: String, expTime: Long) {
+        accessToken = token
+        accessTokenExpirationTime = expTime
+    }
+
+    fun getRefreshToken(): String {
+        if (refreshTokenExpirationTime - System.currentTimeMillis() < 10_000) {
+            refreshToken = ""
+            refreshTokenExpirationTime = 0
+        }
+        return refreshToken?: ""
+    }
+
+    fun setRefreshToken(token: String, expTime: Long) {
+        refreshToken = token
+        refreshTokenExpirationTime = expTime
+    }
 }
