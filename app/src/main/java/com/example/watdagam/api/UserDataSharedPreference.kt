e0eb05cfa80230b84a910403df32d9a92f2e1975
sangkkim <madlogic365@gmail.com>
2023-11-07 13:31:25 +0900

    Feat/WFA-46 사용자 프로필 pref생성

---
+++ app/src/main/java/com/example/watdagam/api/UserDataSharedPreference.kt
@@ -0,0 +1,28 @@
+package com.example.watdagam.api
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class UserDataSharedPreference (context: Context) {
+
+    companion object {
+        private const val PREFERENCE_FILENAME = "user-data-preference"
+        private const val KEY_NICKNAME = "key-nickname"
+        private const val KEY_POSTS = "key-posts"
+        private const val KEY_LIKES = "key-likes"
+    }
+    private val preferences: SharedPreferences = context.getSharedPreferences(PREFERENCE_FILENAME,0)
+
+    var nickname: String?
+        get() = preferences.getString(KEY_NICKNAME, "")
+        set(value) = preferences.edit().putString(KEY_NICKNAME, value).apply()
+
+    var posts: Int
+        get() = preferences.getInt(KEY_POSTS, 0)
+        set(value) = preferences.edit().putInt(KEY_POSTS, value).apply()
+
+    var likes: Int
+        get() = preferences.getInt(KEY_LIKES, 0)
+        set(value) = preferences.edit().putInt(KEY_NICKNAME, value).apply()
+
+}
