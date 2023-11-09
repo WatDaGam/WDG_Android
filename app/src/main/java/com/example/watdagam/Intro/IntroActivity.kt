e4d0eb395bb88626c32971dce62666f09a0d0397
sangkkim <madlogic365@gmail.com>
2023-11-09 15:31:06 +0900

    Feat/WFA-55 인트로 화면 생성

---
+++ app/src/main/java/com/example/watdagam/Intro/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.watdagam.Intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.watdagam.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
