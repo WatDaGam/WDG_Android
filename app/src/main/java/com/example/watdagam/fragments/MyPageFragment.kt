5bfde7cde4b23226bfa5f46e93572464f296fa93
sangkkim <madlogic365@gmail.com>
2023-11-09 14:39:44 +0900

    Refector/WFA-39 API viewmodel 분리

--- app/src/main/java/com/example/watdagam/fragments/MyPageFragment.kt
+++ app/src/main/java/com/example/watdagam/fragments/MyPageFragment.kt
@@ -1,23 +1,16 @@
 package com.example.watdagam.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
-import com.example.watdagam.LoginActivity
 import com.example.watdagam.R
-import com.example.watdagam.api.ApiService
-import com.example.watdagam.api.UserDataSharedPreference
 import com.example.watdagam.databinding.FragmentMyPageBinding
-import retrofit2.Response
 
 class MyPageFragment : Fragment() {
     private lateinit var viewBinding: FragmentMyPageBinding
@@ -30,7 +23,7 @@ class MyPageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         viewBinding = FragmentMyPageBinding.inflate(inflater, container, false)
         model = ViewModelProvider(this)[MyPageFragmentViewModel::class.java]
         model.getProfile().observe(viewLifecycleOwner) {profile ->
@@ -41,33 +34,23 @@ class MyPageFragment : Fragment() {
 
         model.loadProfile(requireContext())
 
-        // Title
-        viewBinding.toolbarTitle.text = "MyPage"
-
         // Menu
         viewBinding.toolbar.overflowIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_menu)
         viewBinding.toolbar.inflateMenu(R.menu.my_page_menu)
         viewBinding.toolbar.setOnMenuItemClickListener { item: MenuItem ->
             when (item.itemId) {
                 R.id.logoutButton -> {
-//                    ApiService.clearUserData()
-                    val intent = Intent(requireContext(), LoginActivity::class.java)
-                    startActivity(intent)
+                    model.logout(requireContext())
                     true
                 }
                 R.id.withdrawalButton -> {
                     val builder = AlertDialog.Builder(requireContext())
                         .setMessage("정말로 회원 탈퇴하시겠습니까?")
                         .setPositiveButton("네") { _, _ ->
-                            val apiService = ApiService.getInstance(requireContext())
-//                            apiService.withdrawal(
-//                                onSuccess = {_, response -> onWithdrawalSuccess(response)},
-//                                onFailure = {_, _ -> onWithdrawalFailure()},
-//                            )
+                            model.withdraw(requireContext())
                         }
                         .setNegativeButton("아니요", null)
-                    val dialog = builder.create()
-                    dialog.show()
+                    builder.create().show()
                     true
                 }
                 else -> false
@@ -81,26 +64,5 @@ class MyPageFragment : Fragment() {
         return viewBinding.root
     }
 
-    private fun onWithdrawalSuccess(response: Response<Void>) {
-        when (response.code()) {
-            200 -> {
-                Toast.makeText(requireContext(), "회원 탈퇴 되었습니다.", Toast.LENGTH_SHORT).show()
-                val intent = Intent(requireContext(), LoginActivity::class.java)
-                startActivity(intent)
-            }
-            400 -> {
-                Toast.makeText(requireContext(), "로그인 정보가 만료되었습니다.", Toast.LENGTH_SHORT).show()
-                val intent = Intent(requireContext(), LoginActivity::class.java)
-                startActivity(intent)
-            }
-            else -> {
-                Log.e("WDG_API", "Unhandled Response code ${response.code()}")
-            }
-        }
-    }
-
-    private fun onWithdrawalFailure() {
-        Toast.makeText(requireContext(), "잠시 후 다시 요청해주세요", Toast.LENGTH_SHORT).show()
-    }
 
 }
--- app/src/main/java/com/example/watdagam/fragments/MyPageFragmentViewModel.kt
+++ app/src/main/java/com/example/watdagam/fragments/MyPageFragmentViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.watdagam.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.watdagam.LoginActivity
 import com.example.watdagam.api.ApiService
 import com.example.watdagam.api.UserInfo
 import kotlinx.coroutines.launch
@@ -43,6 +46,40 @@ class MyPageFragmentViewModel: ViewModel() {
             } catch (e: RuntimeException) {
                 Log.e("WDG_MY_PAGE", e.message ?: "(no error message)")
             }
+        }
+    }
+
+    fun logout(context: Context) {
+        ApiService.token_pref.setAccessToken("", 0)
+        ApiService.token_pref.setRefreshToken("", 0)
+        val intent = Intent(context, LoginActivity::class.java)
+        context.startActivity(intent)
+    }
+
+    fun withdraw(context: Context) {
+        viewModelScope.launch {
+            val apiService = ApiService.getInstance(context)
+            apiService.withdrawal(
+                context,
+                onSuccess = { _, response ->
+                    when (response.code()) {
+                        200 -> {
+                            Toast.makeText(context, "회원 탈퇴 되었습니다.", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(context, LoginActivity::class.java)
+                            context.startActivity(intent)
+                        }
+                        400 -> {
+                            Toast.makeText(context, "로그인 정보가 만료되었습니다.", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(context, LoginActivity::class.java)
+                            context.startActivity(intent)
+                        }
+                        else -> {
+                            Log.e("WDG_API", "Unhandled Response code ${response.code()}")
+                        }
+                    }
+                },
+                onFailure = { _, _ ->  },
+            )
         }
     }
 }
