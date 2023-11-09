6cbcdd4305ebac1b16b17eb1f98ccb319ee812c6
sangkkim <madlogic365@gmail.com>
2023-11-09 20:14:14 +0900

    Chore/WFA-57 라이브러리 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    // Shared Preference 암호화
+    implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
 }
