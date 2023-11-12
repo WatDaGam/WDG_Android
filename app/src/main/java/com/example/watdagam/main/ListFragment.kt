e011779c6d3a6296387c6f0f5784fd50739b6da4
sangkkim <madlogic365@gmail.com>
2023-11-12 16:02:52 +0900

    Feat/WFA-49 list fragment 화면 개발

--- app/src/main/java/com/example/watdagam/main/ListFragment.kt
+++ app/src/main/java/com/example/watdagam/main/ListFragment.kt
@@ -1,14 +1,18 @@
 package com.example.watdagam.main
 
-import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.watdagam.Story
+import com.example.watdagam.StoryAdapter
+import com.example.watdagam.StoryDto
 import com.example.watdagam.databinding.FragmentListBinding
+import java.util.Date
 
 class ListFragment : Fragment() {
     private lateinit var viewBinding: FragmentListBinding
@@ -18,21 +22,58 @@ class ListFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    private val storyDtoList = arrayListOf<StoryDto>(
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 0, 3, "This is a content1, This is a content1, This is a.", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 1, 3, "This is a content2", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 2, 3, "This is a content3", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 3, 3, "This is a content4", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 4, 3, "This is a content5", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 5, 3, "This is a content6", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 6, 3, "This is a content7", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 7, 3, "This is a content8", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 8, 3, "This is a content9", 0),
+        StoryDto(Date(0), 32.1231231, 127.12312312,
+            "yback", 9, 3, "This is a content10", 0),
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         viewBinding = FragmentListBinding.inflate(inflater, container, false)
 
-        model.getCurrentLocation().observe(requireActivity()) { location: Location ->
-            model.updateLocationInfo(requireContext(), location)
-        }
-
         model.getUserLocation().observe(requireActivity()) { wdgLocation: WDGLocation ->
             viewBinding.toolbarPlaceName.text = wdgLocation.locationText
             viewBinding.toolbarGps.text = wdgLocation.coordinate
 //            Toast.makeText(requireContext(), "Location Updated!", Toast.LENGTH_SHORT).show()
         }
+
+        val storyList = mutableListOf<Story>()
+        for (story in storyDtoList) {
+            storyList.add(Story(
+                story.createdAt,
+                story.lati,
+                story.longi,
+                story.nickname,
+                story.id,
+                story.userId,
+                story.content,
+                story.likeNum,
+                0.0
+            ))
+        }
+        viewBinding.storyList.layoutManager = LinearLayoutManager(requireContext())
+        viewBinding.storyList.adapter = StoryAdapter(storyList)
+        viewBinding.storyList.addItemDecoration((DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)))
 
         model.reloadLocation(requireActivity())
 
