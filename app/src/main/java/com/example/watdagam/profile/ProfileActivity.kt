2294310bf18fbab11625b01777c190fcf4a414cb
sangkkim <madlogic365@gmail.com>
2023-11-14 16:37:33 +0900

    Feat/WFA-65 profile 분리

---
+++ app/src/main/java/com/example/watdagam/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.watdagam.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.watdagam.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
