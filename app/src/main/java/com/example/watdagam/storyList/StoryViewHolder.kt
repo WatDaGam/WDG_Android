a0aa46f44d3314f65d6162aae4d2896b50f1d2a5
sangkkim <madlogic365@gmail.com>
2023-11-21 17:04:08 +0900

    Feat/WFA-91 list item 닫힘 애니메이션 삭제

--- app/src/main/java/com/example/watdagam/storyList/StoryViewHolder.kt
+++ app/src/main/java/com/example/watdagam/storyList/StoryViewHolder.kt
@@ -1,14 +1,12 @@
 package com.example.watdagam.storyList
 
-import android.transition.AutoTransition
 import android.transition.Scene
 import android.transition.TransitionManager
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.Transformation
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.watdagam.R
 import com.example.watdagam.api.WDGStoryService
@@ -17,65 +15,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class StoryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    companion object {
-        private const val TAG = "WDG_storyViewHolder"
-    }
-
-    private lateinit var sceneRoot: ViewGroup
-    private lateinit var foldedScene: Scene
-    private lateinit var expandedScene: Scene
-    private val transition = AutoTransition().also { it.duration = 100 }
-
     private lateinit var title: TextView
     private lateinit var location: TextView
     private lateinit var content: TextView
     private lateinit var likes: TextView
     private lateinit var distance: TextView
-
     fun bind(story: StoryItem) {
-        sceneRoot = itemView.findViewById(R.id.scene_root)
-        foldedScene = Scene.getSceneForLayout(sceneRoot, R.layout.scene_story_folded, itemView.context)
-        expandedScene = Scene.getSceneForLayout(sceneRoot, R.layout.scene_story_expanded, itemView.context)
-        if (story.isExpanded) {
-            TransitionManager.go(expandedScene)
-            sceneRoot.layoutParams.height = (240 * sceneRoot.context.resources.displayMetrics.density).toInt()
-        } else {
-            TransitionManager.go(foldedScene)
-            sceneRoot.layoutParams.height = (90 * sceneRoot.context.resources.displayMetrics.density).toInt()
-        }
-
-        rebindView(itemView, story)
-    }
-
-    private fun expandItem(story: StoryItem) {
-        story.isExpanded = true
-        TransitionManager.go(expandedScene, transition)
-        val expandRoot = object: Animation() {
-            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-                sceneRoot.layoutParams.height =
-                    ((90 + 150 * interpolatedTime) * sceneRoot.context.resources.displayMetrics.density).toInt()
-                sceneRoot.requestLayout()
-            }
-        }.also {it.duration = 100}
-        itemView.startAnimation(expandRoot)
-        rebindView(itemView, story)
-    }
-
-    private fun foldItem(story: StoryItem) {
-        story.isExpanded = false
-        TransitionManager.go(foldedScene, transition)
-        val foldRoot = object: Animation() {
-            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-                sceneRoot.layoutParams.height =
-                    ((240 - 150 * interpolatedTime) * sceneRoot.context.resources.displayMetrics.density).toInt()
-                sceneRoot.requestLayout()
-            }
-        }.also {it.duration = 150; it.startOffset = 150}
-        itemView.startAnimation(foldRoot)
-        rebindView(itemView, story)
-    }
-
-    private fun rebindView(itemView: View, story: StoryItem) {
         title = itemView.findViewById(R.id.story_title)
         location = itemView.findViewById(R.id.story_location)
         content = itemView.findViewById(R.id.story_content)
@@ -88,42 +33,61 @@ class StoryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         likes.text = story.likes
         distance.text = story.distance
 
-        if (story.isExpanded) {
-            sceneRoot.setOnClickListener { foldItem(story) }
-            likes.setOnClickListener {
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        val response = WDGStoryService.addLike(itemView.context, story.id)
-                        if (response.isSuccessful) {
-                            story.likes += 1
-                        } else {
-                            throw Exception("Response is not Successful")
+        val sceneRoot: ViewGroup = itemView.findViewById(R.id.scene_root)
+        val expandedScene = Scene.getSceneForLayout(sceneRoot, R.layout.scene_story_expanded, itemView.context).also {
+            it.setEnterAction {
+                title = itemView.findViewById(R.id.story_title)
+                location = itemView.findViewById(R.id.story_location)
+                content = itemView.findViewById(R.id.story_content)
+                likes = itemView.findViewById(R.id.story_likes)
+                distance = itemView.findViewById(R.id.story_distance)
+
+                title.text = story.title
+                location.text = story.location
+                content.text = story.content
+                likes.text = story.likes
+                distance.text = story.distance
+                likes.setOnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        try {
+                            val likeNum = story.likes.toInt()
+                            story.likes = (likeNum + 1).toString()
+                            val response = WDGStoryService.addLike(itemView.context, story.id)
+                            if (response.isSuccessful.not()) {
+                                throw Exception("Response is not Successful")
+                            }
+                        } catch (e: Exception) {
+                            Log.e("WDG_storyViewHolder", "like failed cause ${e.message} ${e.cause}")
                         }
-                    } catch (e: Exception) {
-                        Log.e(TAG, "API Failed cause ${e.message} ${e.cause}")
                     }
                 }
             }
-        } else {
-            sceneRoot.setOnClickListener { expandItem(story) }
         }
-    }
+        val foldedScene = Scene.getSceneForLayout(sceneRoot, R.layout.scene_story_folded, itemView.context).also {
+            it.setEnterAction {
+                title = itemView.findViewById(R.id.story_title)
+                location = itemView.findViewById(R.id.story_location)
+                content = itemView.findViewById(R.id.story_content)
+                likes = itemView.findViewById(R.id.story_likes)
+                distance = itemView.findViewById(R.id.story_distance)
 
-    private fun getLikesString(likes: Int): String {
-        return if (likes > 1_000_000) {
-            String.format("%.3f M", likes / 1_000_000f)
-        } else if (likes > 1_000) {
-            String.format("%.3f K", likes / 1_000f)
-        } else {
-            String.format("%d", likes)
+                title.text = story.title
+                location.text = story.location
+                content.text = story.content
+                likes.text = story.likes
+                distance.text = story.distance
+            }
         }
-    }
 
-    private fun getDistanceString(distance: Double): String {
-        return if (distance > 1_000) {
-            String.format("%.3f km", distance / 1_000)
-        } else {
-            String.format("%.3f m", distance)
+        itemView.setOnClickListener {
+            Toast.makeText(itemView.context, "view click", Toast.LENGTH_SHORT).show()
+            if (story.isExpanded) {
+                story.isExpanded = false
+                TransitionManager.go(foldedScene, null)
+            } else {
+                story.isExpanded = true
+                TransitionManager.go(expandedScene)
+            }
         }
     }
 }
